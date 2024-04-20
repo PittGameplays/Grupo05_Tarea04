c2110c7a7d3df9d741caacfce5abf32c4cd18519
pedro <pedro@LAPTOP-BF96IE7Q.bbrouter>
2024-04-20 01:13:23 -0500

    Grupo 6 tarea 04 ejercicio 01

--- src/Acceso/Salvador.java
+++ src/Acceso/Salvador.java
@@ -1,10 +1,5 @@
 package Acceso;
 
 public class Salvador {
-    private boolean tumamabicho;
-    private int loncansepasasoloxd;
     
-    public void nmmsxd(){
-        tumamabicho = true;
-    }
 }
